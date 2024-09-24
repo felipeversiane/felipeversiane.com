d7f26d8a13e0cfee83da6d8dbdc754de92c924cf
Felipe Fernandes Versiane <felipeversiane09@gmail.com>
2024-09-24 08:20:12 -0300

    fix: adding career props

---
+++ src/config/career.ts
@@ -0,0 +1,9 @@
+export interface CareerProps {
+    title: string,
+    enterprise: string,
+    enterprise_url: string,
+    location: string,
+    begin: string,
+    end: string
+    now: boolean
+}
--- src/config/index.ts
+++ src/config/index.ts
@@ -2,3 +2,4 @@ export * from "./i18n";
 export * from "./social";
 export * from "./project";
 export * from "./personal"
+export * from "./career"
